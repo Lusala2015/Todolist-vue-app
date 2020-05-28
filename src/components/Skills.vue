670e5190e9d8c15df17d7727d7e38e521e883906
Lusala2015 <lusalamark17@gmail.com>
2020-05-28 15:30:15 +0300

    Basic CSS

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,10 +2,10 @@
   <div class="hello">
     <div class="holder">
       <ul>
-        <li v-for="(data, index) in Skills" :key="index">{{index}}.{{data.skill}}</li>
+        <li v-for="(data, index) in Skills" :key="index">{{data.skill}}</li>
       </ul>
 
-      <div v-bind:class="alertObject "></div>
+      <p>These are the skills that you posses.</p>
     </div>
   </div>
 </template>
@@ -29,9 +29,32 @@ data (){
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.alert{
-  background-color: greenyellow;
-  width: 100%;
-  height:30px;
-}
+  .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
 </style>
