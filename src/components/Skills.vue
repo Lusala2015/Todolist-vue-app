fd7c9427357e5e758182088efd436c2769d5898f
Lusala2015 <lusalamark17@gmail.com>
2020-05-28 15:05:54 +0300

    object Binding

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -4,6 +4,8 @@
       <ul>
         <li v-for="(data, index) in Skills" :key="index">{{index}}.{{data.skill}}</li>
       </ul>
+
+      <div v-bind:class="alertObject "></div>
     </div>
   </div>
 </template>
@@ -16,7 +18,10 @@ data (){
    Skills : [
      {"skill" : "vue.js"},
      {"skill" : "Front End Developer"},
-   ]
+   ],
+   alertObject: {
+      alert: true,
+   }
   }
 }
 }
@@ -24,5 +29,9 @@ data (){
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.alert{
+  background-color: greenyellow;
+  width: 100%;
+  height:30px;
+}
 </style>
